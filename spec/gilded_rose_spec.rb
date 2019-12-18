72b84f965b9d8cd3076f41089f0928d525bc1d32
ellieSMASH <eliseaston@gmail.com>
2019-12-18 18:25:56 +0000

    test to check legacy code: ordinary object sellin and quality values decrease 1

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,8 +1,24 @@
 require 'gilded_rose'
+require 'item'
 
 describe GildedRose do
 
+  # let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", nil, 80) }
+  let(:brie) { Item.new("Aged Brie", 5, 20) }
+  let(:backstage) { Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10) }
+  let(:cake) { Item.new("chocolate cake", 3, 10) }
+
+  let(:items) { [brie, backstage, cake] }
+
+  let(:gilded_rose) { described_class.new(items) }
+
   describe "#update_quality" do
+
+    it 'decreases the sell-in and quality value of a normal item by 1' do
+      gilded_rose.update_quality
+      expect(gilded_rose.items[2].sell_in).to eq(2)
+      expect(gilded_rose.items[2].quality).to eq(9)
+    end
 
   end
 
