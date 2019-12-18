14694abec5aa289f454731fc6ea9cbfee5b5666c
ellieSMASH <eliseaston@gmail.com>
2019-12-18 20:36:33 +0000

    refactor new code. Test that items with max and min value do not alter quality values

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,44 +8,36 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-
-      p item.name
-      p item.sell_in
       case
       when item.name == "Aged Brie"
         update_brie(item)
       when item.name.include?("Backstage passes")
         update_backstage(item)
-      when item.name.include?("Sulfuras")
+      when item.name == "Sulfuras, Hand of Ragnaros"
         update_sulfuras(item)
       else
         update_item(item)
       end
-
+      item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
     end
   end
 
   private
 
   def update_brie(item)
-    item.sell_in -= 1
     item.quality += 1 unless item.quality >= 50
   end
 
   def update_backstage(item)
     p "Called update backstage method"
     if item.sell_in <= 0
-      item.sell_in -= 1
       item.quality = 0
     elsif item.sell_in <= 5
-      item.sell_in -= 1
       item.quality += 3 unless item.quality >= 50
     elsif item.sell_in <= 10
-      item.sell_in -= 1
       item.quality += 2 unless item.quality >= 50
     else
-      item.sell_in -= 1
-      item.quality += 1
+      item.quality += 1 unless item.quality >= 50
     end
   end
 
@@ -56,11 +48,9 @@ class GildedRose
 
   def update_item(item)
     if item.sell_in > 0
-      item.sell_in -= 1
       item.quality -= 1 unless item.quality == 0
     else
-      item.sell_in -= 1
-      item.quality -= 2
+      item.quality -= 2 unless item.quality <= 1
     end
   end
 
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,21 +3,20 @@ require 'item'
 
 describe GildedRose do
 
-  # let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", nil, 80) }
   let(:brie) { Item.new("Aged Brie", 5, 20) }
+  let(:quality_brie) { Item.new("Aged Brie", 5, 50) }
   let(:backstage1) { Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10) }
   let(:backstage2) { Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10) }
   let(:backstage3) { Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10) }
   let(:cake) { Item.new("chocolate cake", 1, 10) }
+  let(:bad_cake) { Item.new("bad cake", 5, 0) }
   let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", 0, 80) }
 
-  let(:items) { [brie, backstage1, backstage2, backstage3, cake, sulfuras] }
+  let(:items) { [brie, backstage1, backstage2, backstage3, cake, sulfuras, quality_brie, bad_cake] }
 
   let(:gilded_rose) { described_class.new(items) }
 
   describe "#update_quality" do
-
-    context 'check existing code works' do
 
       it 'decreases the sell-in and quality value of a normal item by 1' do
         gilded_rose.update_quality
@@ -62,8 +61,16 @@ describe GildedRose do
         expect(gilded_rose.items[5].quality).to eq(80)
       end
 
+      it 'does not increase the quality value of brie with quality value of 50' do
+        gilded_rose.update_quality
+        expect(gilded_rose.items[6].quality).to eq(50)
+      end
+
+      it 'does not decrease the quality value of an item wih 0 value' do
+        gilded_rose.update_quality
+        expect(gilded_rose.items[7].quality).to eq(0)
+      end
+
     end
 
   end
-
-end
