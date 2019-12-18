1a9f74e301dfaba02e7ac06eab21016ba7521bf7
ellieSMASH <eliseaston@gmail.com>
2019-12-18 20:06:03 +0000

    refactor update method to use switch statement and separate method for each item type

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,49 +8,60 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+
+      p item.name
+      p item.sell_in
+      case
+      when item.name == "Aged Brie"
+        update_brie(item)
+      when item.name.include?("Backstage passes")
+        update_backstage(item)
+      when item.name.include?("Sulfuras")
+        update_sulfuras(item)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        update_item(item)
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+
     end
   end
+
+  private
+
+  def update_brie(item)
+    item.sell_in -= 1
+    item.quality += 1 unless item.quality >= 50
+  end
+
+  def update_backstage(item)
+    p "Called update backstage method"
+    if item.sell_in <= 0
+      item.sell_in -= 1
+      item.quality = 0
+    elsif item.sell_in <= 5
+      item.sell_in -= 1
+      item.quality += 3 unless item.quality >= 50
+    elsif item.sell_in <= 10
+      item.sell_in -= 1
+      item.quality += 2 unless item.quality >= 50
+    else
+      item.sell_in -= 1
+      item.quality += 1
+    end
+  end
+
+  def update_sulfuras(item)
+    item.sell_in = item.sell_in
+    item.quality = item.quality
+  end
+
+  def update_item(item)
+    if item.sell_in > 0
+      item.sell_in -= 1
+      item.quality -= 1 unless item.quality == 0
+    else
+      item.sell_in -= 1
+      item.quality -= 2
+    end
+  end
+
 end
