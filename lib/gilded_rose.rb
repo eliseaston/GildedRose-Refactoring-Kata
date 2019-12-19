a6cb69f1a322f7f041fd8310cc9930aed175d067
ellieSMASH <eliseaston@gmail.com>
2019-12-19 08:41:45 +0000

    refactor: add check_limits method for max and min quality values

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -11,41 +11,46 @@ class GildedRose
       case
       when item.name == "Aged Brie"
         update_brie(item)
-      when item.name.include?("Backstage passes")
+      when item.name.include?("Backstage")
         update_backstage(item)
       when item.name == "Sulfuras, Hand of Ragnaros"
       else
         update_item(item)
       end
-      item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
+      unless item.name == "Sulfuras, Hand of Ragnaros"
+        item.sell_in -= 1
+        check_limits(item)
+      end
     end
   end
 
   private
 
   def update_brie(item)
-    item.quality += 1 unless item.quality >= 50
+    item.quality += 1
   end
 
   def update_backstage(item)
-    p "Called update backstage method"
     if item.sell_in <= 0
       item.quality = 0
     elsif item.sell_in <= 5
-      item.quality += 3 unless item.quality >= 50
+      item.quality += 3
     elsif item.sell_in <= 10
-      item.quality += 2 unless item.quality >= 50
+      item.quality += 2
     else
-      item.quality += 1 unless item.quality >= 50
+      item.quality += 1
     end
   end
 
-
   def update_item(item)
-    if item.sell_in > 0
-      item.quality -= 1 unless item.quality == 0
-    else
-      item.quality -= 2 unless item.quality <= 1
+    item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
+  end
+
+  def check_limits(item)
+    if item.quality > 50
+      item.quality = 50
+    elsif item.quality < 0
+      item.quality = 0
     end
   end
 
