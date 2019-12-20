c6ffa768657843d6e8989ef87615dad92a46728e
ellieSMASH <eliseaston@gmail.com>
2019-12-20 11:45:04 +0000

    unit tests for BasicItem class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,9 +2,22 @@ require 'item'
 
 describe Item do
 
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    expect(items[0].name).to eq "foo"
+  # it "does not change the name" do
+  #   items = [Item.new("foo", 0, 0)]
+  #   expect(items[0].name).to eq "foo"
+  # end
+
+  context 'BasicItem' do
+    let(:basic_item) { BasicItem.new("Cake", 5, 10)}
+
+    it 'decreases basic item quality value by 1' do
+      expect(basic_item.update_quality).to eq(9)
+    end
+
+    it 'decreases basic item sell in value by 1' do
+      expect(basic_item.update_sell_in).to eq(4)
+    end
+
   end
 
 end
