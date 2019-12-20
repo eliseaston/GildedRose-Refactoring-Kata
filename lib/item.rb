91d905f8934061d1123bb13582b5e81bff4e5f84
ellieSMASH <eliseaston@gmail.com>
2019-12-20 11:27:47 +0000

    refactor update methods into new Item subclasses

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,8 @@
 class Item
+
+  MIN_VALUE = 0
+  MAX_VALUE = 50
+
   attr_accessor :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
@@ -12,31 +16,86 @@ class Item
   end
 end
 
-class BasicItem < Item
-  def update
 
+class BasicItem < Item
+  def update_quality
+    @sell_in > 0 ? @quality -= 1 : @quality -= 2
+  end
+
+  def update_sell_in
+    @sell_in -= 1
+  end
+
+  def check_limits
+    @quality = MAX_VALUE if @quality > MAX_VALUE
+    @quality = MIN_VALUE if @quality < MIN_VALUE
   end
 end
+
 
 class BackstagePass < Item
-  def update
+  def update_quality
+    if @sell_in <= 0
+      @quality = 0
+    elsif @sell_in <= 5
+      @quality += 3
+    elsif @sell_in <= 10
+      @quality += 2
+    else
+      @quality += 1
+    end
+  end
+
+  def update_sell_in
+    @sell_in -= 1
+  end
+
+  def check_limits
+    @quality = MAX_VALUE if @quality > MAX_VALUE
+    @quality = MIN_VALUE if @quality < MIN_VALUE
   end
 end
+
 
 class Brie < Item
-  def update
+  def update_quality
+    @quality += 1
+  end
 
+  def update_sell_in
+    @sell_in -= 1
+  end
+
+  def check_limits
+    @quality = MAX_VALUE if @quality > MAX_VALUE
+    @quality = MIN_VALUE if @quality < MIN_VALUE
   end
 end
+
 
 class Sulfuras < Item
-  def update
+  def update_quality
+  end
 
+  def update_sell_in
+  end
+
+  def check_limits
   end
 end
 
+
 class ConjuredItem < Item
-  def update
-    
+  def update_quality
+    @sell_in > 0 ? @quality -= 2 : @quality -= 4
+  end
+
+  def update_sell_in
+    @sell_in -= 1
+  end
+
+  def check_limits
+    @quality = MAX_VALUE if @quality > MAX_VALUE
+    @quality = MIN_VALUE if @quality < MIN_VALUE
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,16 +3,16 @@ require 'item'
 
 describe GildedRose do
 
-  let(:brie) { Item.new("Aged Brie", 5, 20) }
-  let(:quality_brie) { Item.new("Aged Brie", 5, 50) }
-  let(:backstage1) { Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10) }
-  let(:backstage2) { Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10) }
-  let(:backstage3) { Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10) }
-  let(:cake) { Item.new("chocolate cake", 1, 10) }
-  let(:bad_cake) { Item.new("bad cake", 5, 0) }
-  let(:sulfuras) { Item.new("Sulfuras, Hand of Ragnaros", 0, 80) }
-  let(:conjured) { Item.new("Conjured item", 5, 10) }
-  let(:conjured2) { Item.new("Conjured item", 0, 10) }
+  let(:brie) { Brie.new("Aged Brie", 5, 20) }
+  let(:quality_brie) { Brie.new("Aged Brie", 5, 50) }
+  let(:backstage1) { BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 10, 10) }
+  let(:backstage2) { BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 5, 10) }
+  let(:backstage3) { BackstagePass.new("Backstage passes to a TAFKAL80ETC concert", 0, 10) }
+  let(:cake) { BasicItem.new("chocolate cake", 1, 10) }
+  let(:bad_cake) { BasicItem.new("bad cake", 5, 0) }
+  let(:sulfuras) { Sulfuras.new("Sulfuras, Hand of Ragnaros", 0, 80) }
+  let(:conjured) { ConjuredItem.new("Conjured item", 5, 10) }
+  let(:conjured2) { ConjuredItem.new("Conjured item", 0, 10) }
 
   let(:items) { [brie, backstage1, backstage2, backstage3, cake, sulfuras, quality_brie, bad_cake, conjured, conjured2] }
 
@@ -22,21 +22,21 @@ describe GildedRose do
 
       it 'decreases the sell-in and quality value of a normal item by 1' do
         cake = Item.new("Cake", 5, 20)
-        gilded_rose = GildedRose.new([cake])
-        expect(gilded_rose.update_quality).to eq("Cake, 4, 19")
-        # expect(gilded_rose.items[4].sell_in).to eq(0)
-        # expect(gilded_rose.items[4].quality).to eq(9)
+        # gilded_rose = GildedRose.new([cake])
+        gilded_rose.update
+        expect(gilded_rose.items[4].sell_in).to eq(0)
+        expect(gilded_rose.items[4].quality).to eq(9)
       end
 
       it 'decreases the quality value of a normal item twice as much after sell in date' do
-        gilded_rose.update_quality
-        gilded_rose.update_quality
+        gilded_rose.update
+        gilded_rose.update
         expect(gilded_rose.items[4].sell_in).to eq(-1)
         expect(gilded_rose.items[4].quality).to eq(7)
       end
 
       it 'does not decrease the quality value of an item wih 0 value' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[7].quality).to eq(0)
       end
 
@@ -45,13 +45,13 @@ describe GildedRose do
     context 'brie' do
 
       it 'decreases the sell-in and increases the quality value of brie' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[0].sell_in).to eq(4)
         expect(gilded_rose.items[0].quality).to eq(21)
       end
 
       it 'does not increase the quality value of brie with quality value of 50' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[6].quality).to eq(50)
       end
 
@@ -60,19 +60,19 @@ describe GildedRose do
     context 'backstage passes' do
 
       it 'decreases the sell-in and increases by 2 the quality of backstage pass less than 10 days before gig' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[1].sell_in).to eq(9)
         expect(gilded_rose.items[1].quality).to eq(12)
       end
 
       it 'decreases the sell-in and increases by 3 the quality of backstage pass less than 5 days before gig' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[2].sell_in).to eq(4)
         expect(gilded_rose.items[2].quality).to eq(13)
       end
 
       it 'decreases the sell-in and reduces the quality to 0 of a backstage pass after the gig' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[3].sell_in).to eq(-1)
         expect(gilded_rose.items[3].quality).to eq(0)
       end
@@ -82,7 +82,7 @@ describe GildedRose do
     context 'sulfuras' do
 
       it 'does not decrease the sell-in or quality values of Sulfuras' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[5].sell_in).to eq(0)
         expect(gilded_rose.items[5].quality).to eq(80)
       end
@@ -92,21 +92,21 @@ describe GildedRose do
     context 'conjured items' do
 
       it 'decreases conjured items quality value by 2 before sell-by date' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[8].quality).to eq(8)
       end
 
       it 'decreases conjured items quality value by 4 after sell-by date' do
-        gilded_rose.update_quality
+        gilded_rose.update
         expect(gilded_rose.items[9].quality).to eq(6)
       end
 
       it 'does not decrease conjured items value below 0' do
-        gilded_rose.update_quality
-        gilded_rose.update_quality
-        gilded_rose.update_quality
-        gilded_rose.update_quality
-        gilded_rose.update_quality
+        gilded_rose.update
+        gilded_rose.update
+        gilded_rose.update
+        gilded_rose.update
+        gilded_rose.update
         expect(gilded_rose.items[9].quality).to eq(0)
       end
 
